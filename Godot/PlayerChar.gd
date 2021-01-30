dfa276011d718bbeb330994e6846a3a579a4ed0f
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2021-01-30 17:50:17 +0200

    added more audio/visual

--- Godot/PlayerChar.gd
+++ Godot/PlayerChar.gd
@@ -10,7 +10,7 @@ func _physics_process(delta):
 	
 	if wavePushed:
 		vel.x = 0
-		vel.y = 350
+		vel.y = 200
 		move_and_slide(vel, Vector2.UP)
 	
 	else:
@@ -34,12 +34,19 @@ func _physics_process(delta):
 		#move_and_collide(vel * delta)
 	
 		sprite.play("run")
-
+	#animation
+	if (!wavePushed && ((vel.y != 0)||(vel.x != 0))):
+		sprite.play("run")
+	
+	else:
+		sprite.play("idle")
 
 func _on_Wave_body_entered(body):
+	$Drowning.play()
 	wavePushed = true
 
 
 func _on_Wave_body_exited(body):
+	$Drowning.stop()
 	wavePushed = false
 
