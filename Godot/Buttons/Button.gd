7c41dbba0ae05d5c80797681a902c81f08f416cd
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2021-01-30 15:50:08 +0200

    Fixed a crash on startup

--- Godot/Buttons/Button.gd
+++ Godot/Buttons/Button.gd
@@ -4,7 +4,6 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var animation = owner.get_node("FadeIn").get_node("FadeInAnimation")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
